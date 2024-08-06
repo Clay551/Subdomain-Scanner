669f8a950c1e7690b529090ecedacc04c7681590
Clay <156083036+Clay551@users.noreply.github.com>
2024-08-06 04:39:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pyfiglet
 from colorama import Fore
 import colorama
 import os
-os.system(f'cls' if {os.name} == 'nt' else 'clear')
+
+os.system('cls' if os.name == 'nt' else 'clear')
 
 def check_subdomain(subdomain, domain):
     try:
@@ -11,16 +12,18 @@ def check_subdomain(subdomain, domain):
         ip = socket.gethostbyname(subdomain_domain)
         print(colorama.Fore.GREEN)
         print(f"Subdomain found: {subdomain_domain} - IP: {ip}")
+        with open("subdomains_found.txt", "a") as file:
+            file.write(f"Subdomain found: {subdomain_domain} - IP: {ip}\n")
         print(colorama.Fore.RESET)
     except socket.gaierror:
-      
         print(colorama.Fore.RED)
         print(f"Subdomain not found: {subdomain_domain}")
         print(colorama.Fore.RESET)
+
 print(colorama.Fore.RED)
 pyfiglet.print_figlet("Asylum")
 print(colorama.Fore.RESET)
-print("e.g : example.com")
+print("e.g: example.com")
 print('')
 domain = input("Enter Domain==> ")
 
